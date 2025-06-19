30fe805cf6fd007b822fdb4de0c1b0297c94ba21
FelipeMDantas <felipemdantas@terra.com.br>
2025-06-19 19:20:23 -0300

    [feat] Structure the CompanionCard component

--- app/page.tsx
+++ app/page.tsx
@@ -8,9 +8,30 @@ const Page = () => {
       <h1>Popular Companions</h1>
 
       <section className="home-section">
-        <CompanionCard />
-        <CompanionCard />
-        <CompanionCard />
+        <CompanionCard
+          id="123"
+          name="Neura the Brainy Explorer"
+          topic="Neural Network of the Brain"
+          subject="science"
+          duration={45}
+          color="#ffda6e"
+        />
+        <CompanionCard
+          id="456"
+          name="Countsy the Number Wizard"
+          topic="Derivatives & Integrals"
+          subject="maths"
+          duration={30}
+          color="#e5d0ff"
+        />
+        <CompanionCard
+          id="789"
+          name="Verba the Vocabulary Builder"
+          topic="English Literature"
+          subject="language"
+          duration={30}
+          color="#bde7ff"
+        />
       </section>
 
       <section className="home-section">
--- components/CompanionCard.tsx
+++ components/CompanionCard.tsx
@@ -1,5 +1,37 @@
-const CompanionCard = () => {
-  return <div>CompanionCard</div>;
+import Image from "next/image";
+
+interface CompanionCardProps {
+  id: string;
+  name: string;
+  topic: string;
+  subject: string;
+  duration: number;
+  color: string;
+}
+
+const CompanionCard = ({
+  id,
+  name,
+  topic,
+  subject,
+  duration,
+  color,
+}: CompanionCardProps) => {
+  return (
+    <article className="companion-card" style={{ backgroundColor: color }}>
+      <div className="flex justify-between items-center">
+        <div className="subject-badge">{subject}</div>
+        <button className="companion-bookmark">
+          <Image
+            src="/icons/bookmark.svg"
+            alt="bookmark"
+            width={12.5}
+            height={15}
+          />
+        </button>
+      </div>
+    </article>
+  );
 };
 
 export default CompanionCard;
