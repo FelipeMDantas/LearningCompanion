6e2c86e8ea2a2cc4bd6857e49bb7f2dce237e751
FelipeMDantas <felipemdantas@terra.com.br>
2025-06-20 19:39:12 -0300

    [feat] Style the CompanionCard component

--- components/CompanionCard.tsx
+++ components/CompanionCard.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 
 interface CompanionCardProps {
   id: string;
@@ -30,6 +31,24 @@ const CompanionCard = ({
           />
         </button>
       </div>
+
+      <h2 className="text-2xl font-bold">{name}</h2>
+      <p className="text-sm">{topic}</p>
+      <div className="flex items-center gap-2">
+        <Image
+          src="/icons/clock.svg"
+          alt="duration"
+          width={13.5}
+          height={13.5}
+        />
+        <p className="text-sm">{duration} minutes</p>
+      </div>
+
+      <Link href={`/companions/${id}`} className="w-full">
+        <button className="btn-primary w-full justify-center">
+          Launch Lesson
+        </button>
+      </Link>
     </article>
   );
 };
