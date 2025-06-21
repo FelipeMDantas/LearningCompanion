46a0389cb35ffbbab7cdaee480c7ff35dd3524bf
FelipeMDantas <felipemdantas@terra.com.br>
2025-06-21 19:57:57 -0300

    [feat] Structure the CompanionsList component with shadcn's table

--- components/CompanionsList.tsx
+++ components/CompanionsList.tsx
@@ -1,5 +1,61 @@
-const CompanionsList = () => {
-  return <div>CompanionsList</div>;
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { cn } from "@/lib/utils";
+import Image from "next/image";
+import Link from "next/link";
+
+interface CompanionsListProps {
+  title: string;
+  companions?: Companion[];
+  classNames?: string;
+}
+
+const CompanionsList = ({
+  title,
+  companions,
+  classNames,
+}: CompanionsListProps) => {
+  return (
+    <article className={cn("companion-list", classNames)}>
+      <h2 className="font-bold text-3xl">Recent Sessions</h2>
+
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHead className="text-lg w-2/3">Lessons</TableHead>
+            <TableHead className="text-lg">Subject</TableHead>
+            <TableHead className="text-lg">Duration</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {companions?.map(({ id, subject, name, topic, duration }) => (
+            <TableRow key={id}>
+              <TableCell>
+                <Link href={`/companions/${id}`}>
+                  <div className="flex items-center gap-2">
+                    <div>
+                      <Image
+                        src={`/icons/${subject}.svg`}
+                        alt={subject}
+                        width={35}
+                        height={35}
+                      />
+                    </div>
+                  </div>
+                </Link>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </article>
+  );
 };
 
 export default CompanionsList;
