45c3427a0ffc764db9517f64428d3523b630385c
FelipeMDantas <felipemdantas@terra.com.br>
2025-06-23 19:08:52 -0300

    [feat] Structure the CTA component

--- components/CTA.tsx
+++ components/CTA.tsx
@@ -1,5 +1,24 @@
+import Image from "next/image";
+import Link from "next/link";
+
 const CTA = () => {
-  return <div>CTA</div>;
+  return (
+    <section className="cta-section">
+      <div className="cta-badge">Start Learning your way</div>
+      <h2 className="cta-badge">Build and Personalize a Learning Companion</h2>
+      <p>
+        Pick a name, subject, voice & personality - and start learning through
+        voice conversations that feel natural and fun
+      </p>
+      <Image src="/images/cta.svg" alt="cta" width={362} height={362} />
+      <button className="btn-primary">
+        <Image src="/icons/plus.svg" alt="plus" width={12} height={12} />
+        <Link href="/companions/new">
+          <p>Build a New Companion</p>
+        </Link>
+      </button>
+    </section>
+  );
 };
 
 export default CTA;
