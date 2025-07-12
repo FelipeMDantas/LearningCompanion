5a96a33d104517d7574060670dfba13a51195189
FelipeMDantas <felipemdantas@terra.com.br>
2025-07-12 19:53:37 -0300

    [feat] Handle possible Vapi statuses

--- components/CompanionComponent.tsx
+++ components/CompanionComponent.tsx
@@ -1,13 +1,16 @@
 "use client";
 
 import { cn, getSubjectColor } from "@/lib/utils";
-import { useEffect, useState } from "react";
+import { vapi } from "@/lib/vapi.sdk";
+import { LottieRefCurrentProps } from "lottie-react";
+import Image from "next/image";
+import { useEffect, useRef, useState } from "react";
 
 enum CallStatus {
   INACTIVE = "INACTIVE",
   CONNECTING = "CONNECTING",
   ACTIVE = "ACTIVE",
-  FINISHED = "INACTIVE",
+  FINISHED = "FINISHED",
 }
 
 const CompanionComponent = ({
@@ -21,13 +24,35 @@ const CompanionComponent = ({
   voice,
 }: CompanionComponentProps) => {
   const [callStatus, setCallStatus] = useState<CallStatus>(CallStatus.INACTIVE);
+  const [isSpeaking, setIsSpeaking] = useState(false);
+
+  const lottieRef = useRef<LottieRefCurrentProps>(null);
 
   useEffect(() => {
     const onCallStart = () => setCallStatus(CallStatus.ACTIVE);
     const onCallEnd = () => setCallStatus(CallStatus.FINISHED);
-    const onMessage = {};
+    const onMessage = () => {};
+    const onSpeachStart = () => setIsSpeaking(true);
+    const onSpeachEnd = () => setIsSpeaking(false);
     const oneError = (error: Error) => console.log("Error", error);
+
+    vapi.on("call-start", onCallStart);
+    vapi.on("call-end", onCallEnd);
+    vapi.on("message", onMessage);
+    vapi.on("error", oneError);
+    vapi.on("speech-start", onSpeachStart);
+    vapi.on("speech-end", onSpeachEnd);
+
+    return () => {
+      vapi.off("call-start", onCallStart);
+      vapi.off("call-end", onCallEnd);
+      vapi.off("message", onMessage);
+      vapi.off("error", oneError);
+      vapi.off("speech-start", onSpeachStart);
+      vapi.off("speech-end", onSpeachEnd);
+    };
   }, []);
+
   return (
     <section className="flex flex-col h-[70vh]">
       <section className="flex gap-8 max-sm:flex-col">
@@ -37,7 +62,30 @@ const CompanionComponent = ({
             style={{ backgroundColor: getSubjectColor(subject) }}
           >
             <div
-              className={cn("absolute transition-opacity duration-1000")}
+              className={cn(
+                "absolute transition-opacity duration-1000",
+                callStatus === CallStatus.FINISHED ||
+                  callStatus === CallStatus.INACTIVE
+                  ? "opacity-100"
+                  : "opacity-0",
+                callStatus === CallStatus.CONNECTING &&
+                  "opacity-100 animate-pulse"
+              )}
+            >
+              <Image
+                src={`/icons/${subject}.svg`}
+                alt={subject}
+                width={150}
+                height={150}
+                className="max-sm:w-fit"
+              />
+            </div>
+
+            <div
+              className={cn(
+                "absolute transition-opacity duration-1000",
+                callStatus === CallStatus.ACTIVE ? "opacity-100" : "opacity-0"
+              )}
             ></div>
           </div>
         </div>
