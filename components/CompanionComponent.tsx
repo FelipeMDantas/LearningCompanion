c7a415f2a17773d850603f5f75388f4ae52c8754
FelipeMDantas <felipemdantas@terra.com.br>
2025-07-23 17:48:58 -0300

    [feat] Retrieve and show actual companions data

--- components/CompanionComponent.tsx
+++ components/CompanionComponent.tsx
@@ -6,6 +6,7 @@ import Lottie, { LottieRefCurrentProps } from "lottie-react";
 import Image from "next/image";
 import { useEffect, useRef, useState } from "react";
 import soundwaves from "@/constants/soundwaves.json";
+import { addToSessionsHistory } from "@/lib/actions/companion.actions";
 
 enum CallStatus {
   INACTIVE = "INACTIVE",
@@ -43,13 +44,19 @@ const CompanionComponent = ({
 
   useEffect(() => {
     const onCallStart = () => setCallStatus(CallStatus.ACTIVE);
-    const onCallEnd = () => setCallStatus(CallStatus.FINISHED);
+
+    const onCallEnd = () => {
+      setCallStatus(CallStatus.FINISHED);
+      addToSessionsHistory(companionId);
+    };
+
     const onMessage = (message: Message) => {
       if (message.type === "transcript" && message.transcriptType === "final") {
         const newMessage = { role: message.role, content: message.transcript };
         setMessages((prev) => [newMessage, ...prev]);
       }
     };
+
     const onSpeachStart = () => setIsSpeaking(true);
     const onSpeachEnd = () => setIsSpeaking(false);
     const onError = (error: Error) => console.log("Error", error);
