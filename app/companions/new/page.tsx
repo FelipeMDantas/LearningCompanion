4348d4545d8c11615047b311bf7e10b236661a8a
FelipeMDantas <felipemdantas@terra.com.br>
2025-07-31 20:23:55 -0300

    [fix] Mend ESLint build errors

--- app/companions/new/page.tsx
+++ app/companions/new/page.tsx
@@ -29,10 +29,10 @@ const NewCompanion = async () => {
           />
 
           <div className="cta-badge">Upgrade your plan</div>
-          <h1>You've Reached Your Limit</h1>
+          <h1>{`You've Reached Your Limit`}</h1>
           <p>
-            You've reached your companion limit. Upgrade to create more
-            companions and premium features.
+            {`You've reached your companion limit. Upgrade to create more
+            companions and premium features.`}
           </p>
           <Link
             href="/subscription"
